5e13b585848cc04ec211900de503e64b0a9a008d
Derrick Kim <dwkim@3billion.io>
2022-08-21 15:37:41 +0900

    backtest

---
+++ quantata/backtest.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+
+def calc_signal(
+    ratios,
+    buy_th,
+    sell_th,
+    buy_weight=1,
+    sell_weight=10,
+    trade_unit=10,
+    buy_at_high_ratio=True,
+):
+    signals = list()
+    buy_intensity = buy_weight * trade_unit / np.std(ratios)
+    sell_intensity = sell_weight * trade_unit / np.std(ratios)
+
+    for ratio in ratios:
+        if buy_at_high_ratio:
+            if ratio >= buy_th:
+                volume = np.floor((ratio - buy_th) * buy_intensity)
+                signals.append(volume)
+            elif ratio < sell_th:
+                volume = np.floor((sell_th - ratio) * sell_intensity)
+                signals.append(-volume)
+
+        else:
+            if ratio <= buy_th:
+                volume = np.floor((buy_th - ratio) * buy_intensity)
+                signals.append(volume)
+            elif ratio > sell_th:
+                volume = np.floor((ratio - sell_th) * sell_intensity)
+                signals.append(-volume)
+
+    return np.array(signals, np.int64)
+
+
+def simulate(
+    prices, signals, unlimited=True, init_cap=0, init_pos=0, fee_rate=0.001
+):
+    amounts, volumes, fees, poses, caps = list(), list(), list(), list(), list()
+    pos, cap = init_pos, init_cap
+
+    for price, signal in zip(prices, signals):
+        volume = abs(signal)
+        amount = volume * price
+        fee = np.ceil(amount * fee_rate)
+
+        if signal > 0 and (unlimited or cap >= amount + fee):
+            cap -= amount + fee
+            pos += volume
+            amounts.append(amount)
+            volumes.append(volume)
+            fees.append(fee)
+            poses.append(pos)
+            caps.append(cap)
+
+        elif pos >= volume and (
+            cap >= fee or amount >= fee or cap + amount >= fee
+        ):
+            cap += amount - fee
+            pos -= volume
+            amounts.append(-amount)
+            volumes.append(-volume)
+            fees.append(fee)
+            poses.append(pos)
+            caps.append(cap)
+
+        else:
+            amounts.append(0)
+            volumes.append(0)
+            fees.append(0)
+            poses.append(pos)
+            caps.append(cap)
+
+    return (
+        np.array(amounts),
+        np.array(volumes, np.int64),
+        np.array(fees, np.int64),
+        np.array(poses, np.int64),
+        np.array(caps),
+    )
+
+
+def calc_asset(prices, poses, caps):
+    return caps + poses * prices
+
+
+def calc_return(caps):
+    return (caps[-1] - caps[0]) / caps[0]
